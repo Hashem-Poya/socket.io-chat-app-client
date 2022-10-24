719b93c7a36830905abdba5fd19fab41f48d2efb
Hashem-Poya <poya.kpu.2020@gmail.com>
2022-10-24 11:25:57 +0430

    [ADD] Added displaying active users.

--- src/components/ChatSideBar.js
+++ src/components/ChatSideBar.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const ChatSideBar = () => {
+const ChatSideBar = ({ socket }) => {
+  const [activeUsers, setActiveUsers] = useState([]);
+
+  useEffect(() => {
+    socket.on('newUserResponse', (user) => {
+      setActiveUsers(user);
+    });
+  }, [socket, activeUsers]);
+  console.log('activeUsers =>', activeUsers);
+
   return (
     <div className='chat__sidebar'>
       <h2>Open chat</h2>
       <div>
         <h4 className='chat__header'>Active users</h4>
         <div className='chat__users'>
-          <p>User 1</p>
-          <p>User 2</p>
-          <p>User 3</p>
-          <p>User 4</p>
+          {activeUsers.map((user) => (
+            <div key={user.socketId}>
+              <p>{user.username}</p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
