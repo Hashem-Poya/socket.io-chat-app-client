180a2b2297c63bb2ade8b4085e7e54a44b7f5283
Hashem-Poya <poya.kpu.2020@gmail.com>
2022-10-24 11:34:22 +0430

    [ADD] Added auto-scrolling in message.

--- src/pages/ChatPage.js
+++ src/pages/ChatPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 
 import ChatSideBar from '../components/ChatSideBar';
 import ChatBody from '../components/ChatBody';
@@ -6,18 +6,22 @@ import ChatFooter from '../components/ChatFooter';
 
 const ChatPage = ({ socket }) => {
   const [messages, setMessages] = useState([]);
-
+  const lastMessageRef = useRef(null);
   useEffect(() => {
     socket.on('messageResponse', (response) => {
       setMessages([...messages, response]);
     });
   }, [messages, socket]);
 
+  useEffect(() => {
+    lastMessageRef.current?.scrollIntoView({ behavior: 'smooth' });
+  }, [messages]);
+
   return (
     <div className='chat'>
       <ChatSideBar socket={socket} />
       <div className='chat__main'>
-        <ChatBody messages={messages} />
+        <ChatBody messages={messages} lastMessageRef={lastMessageRef} />
         <ChatFooter socket={socket} />
       </div>
     </div>
