49166c9d477bdca5455ec3f97ffa1c95945419d6
Hashem-Poya <poya.kpu.2020@gmail.com>
2022-10-24 11:03:27 +0430

    [ADD] Added listen event for chat response and passed as props to chat body.

--- src/pages/ChatPage.js
+++ src/pages/ChatPage.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import ChatSideBar from '../components/ChatSideBar';
 import ChatBody from '../components/ChatBody';
 import ChatFooter from '../components/ChatFooter';
 
 const ChatPage = ({ socket }) => {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    socket.on('messageResponse', (response) => {
+      setMessages([...messages, ...response]);
+    });
+  }, [messages, socket]);
+
   return (
     <div className='chat'>
       <ChatSideBar />
       <div className='chat__main'>
-        <ChatBody />
+        <ChatBody messages={messages} />
         <ChatFooter socket={socket} />
       </div>
     </div>
